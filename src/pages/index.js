418cde7dfe414070b53cb6cf57971d57b17cc83a
Eduardo Rubio <eduardo.rubio.jr85@gmail.com>
2020-03-19 13:12:20 -0400

    Changed color scheme and started the hero animation

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,17 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
+import Hero from "../components/Hero"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
+  <>
   <Layout>
     <SEO title="Home" />
-    <Hero/>
     <h1>Hello world I am hard coded</h1>
   </Layout>
+  <Hero/>
+  </>
 );
 
 export default IndexPage
