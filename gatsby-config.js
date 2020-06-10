0b15bb6bd1bc033dba544557909d847a8e6e088a
Eduardo Rubio <eduardo.rubio.jr85@gmail.com>
2020-06-10 12:03:08 -0400

    sorting some env issues

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,8 +35,8 @@ module.exports = {
     {
       resolve: `gatsby-source-contentful`,
       options: {
-        spaceId: process.env.GATSBY_SPACE_ID,
-        accessToken: process.env.GATSBY_CONTENT_DELIVERY_API,
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
