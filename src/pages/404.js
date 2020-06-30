565e49fa0631bc7b2cf30a7c39f195000841abee
Eduardo Rubio <eduardo.rubio.jr85@gmail.com>
2020-06-30 11:37:29 -0400

    finished the 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,22 @@
-import React from "react"
+import React, { useEffect } from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import HumansImage from "../images/404-humans.png"
 
-const NotFoundPage = () => (
+import './page-styling/404.scss'
+
+const NotFoundPage = () => {
+
+  return (
   <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <SEO title="not-found: Not found" />
+    <div className="not-found__container">
+      <h1 className="not-found__title">404</h1>
+      <p className="not-found__content">Yikes! Looks like the page you're looking for isn't here... This is so awkward... Go to the menu and try something else?</p>
+      <img className="not-found__img" src={HumansImage} alt='Humans image'/>
+    </div>
   </Layout>
-)
+)}
 
 export default NotFoundPage
