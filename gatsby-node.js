4d2f004f36622c5784040e1edf7f990a240d5c23
Eduardo Rubio <eduardo.rubio.jr85@gmail.com>
2020-07-07 15:31:46 -0400

    Finished the filter logic -- working. Time to style the page

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,16 +1,6 @@
 exports.createPages = async function ({ actions, graphql }) { 
   const { data } = await graphql(`
     query {
-      allContentfulSubGallery {
-        edges {
-          node {
-            slug
-            gallery {
-              slug
-            }
-          }
-        }
-      }
       allContentfulGallery { 
         edges { 
           node { 
@@ -27,18 +17,6 @@ exports.createPages = async function ({ actions, graphql }) {
     path: `/portfolio/${slug}`, 
     component: require.resolve(`./src/templates/Gallery/Gallery.js`),
     context: {slug: slug }, 
-    })
-  })
-  
-  /* Create all the sub-gallery pages */ 
-  data.allContentfulSubGallery.edges.forEach((edge) => { 
-    const gallery_slug = edge.node.gallery.slug;
-    const sub_gallery_slug = edge.node.slug; 
-    const slug = `${gallery_slug}/${sub_gallery_slug}`;
-    actions.createPage({
-      path: `/portfolio/${slug}`,
-      component: require.resolve(`./src/templates/SubGallery/SubGallery.js`),
-      context: { slug: sub_gallery_slug }, 
     })
   })
 }
