1e46dbf5003b25de96968ae7cc1b31fde34bc804
Eduardo Rubio <eduardo.rubio.jr85@gmail.com>
2020-07-24 17:38:43 -0400

    Hot fix for performance on FeatureBanner

--- src/components/FeaturedImageBannerL/FeaturedImageBannerL.js
+++ src/components/FeaturedImageBannerL/FeaturedImageBannerL.js
@@ -1,52 +1,32 @@
-import React, { useState, useEffect } from "react"
+import React from "react"
+import Media from "react-media"
 import Img from "gatsby-image"
 import "./FeaturedImageBannerL.scss"
 
 const FeaturedImageBannerL = props => {
-  function getWindowDimensions() {
-    const { innerWidth: width } = window
-    return {
-      width,
-    }
-  }
-
-  function useWindowDimensions() {
-    const [windowDimensions, setWindowDimensions] = useState(
-      getWindowDimensions()
-    )
-
-    useEffect(() => {
-      function handleResize() {
-        setWindowDimensions(getWindowDimensions())
-      }
-      window.addEventListener("resize", handleResize)
-      return () => window.removeEventListener("resize", handleResize)
-    }, [])
-    return windowDimensions
-  }
-
-  const { width } = useWindowDimensions()
   const centerPiece = props.data.edges[0].node.centerPiece.fluid
   const topleft = props.data.edges[0].node.sideImages[0].fluid
   const bottomleft = props.data.edges[0].node.sideImages[1].fluid
   const topright = props.data.edges[0].node.sideImages[2].fluid
   const bottomright = props.data.edges[0].node.sideImages[3].fluid
 
-  const bannerImages =
-    width > 860
-      ? [
-          <Img fluid={topleft} className="top-left" />,
-          <Img fluid={bottomleft} className="bottom-left" />,
-          <Img
-            fluid={centerPiece}
-            className="FeaturedImageBannerL__image cp"
-          />,
-          <Img fluid={topright} className="top-right" />,
-          <Img fluid={bottomright} className="bottom-right" />,
-        ]
-      : [<Img fluid={centerPiece} className="FeaturedImageBannerL__image cp" />]
-
-  return <div className="FeaturedImageBannerL__container">{bannerImages}</div>
+  return (
+    <Media queries={{ small: { maxWidth: 860 } }}>
+      {matches => matches.small ? (
+        <div className="FeaturedImageBannerL__container">
+          <Img fluid={centerPiece} className="FeaturedImageBannerL__image cp" />
+        </div>
+      ) : (
+        <div className="FeaturedImageBannerL__container">
+          <Img fluid={topleft} className="top-left" />
+          <Img fluid={bottomleft} className="bottom-left" />
+          <Img fluid={centerPiece} className="FeaturedImageBannerL__image cp" />
+          <Img fluid={topright} className="top-right" />
+          <Img fluid={bottomright} className="bottom-right" />
+        </div>
+      )}
+    </Media>
+  )
 }
 
 export default FeaturedImageBannerL
