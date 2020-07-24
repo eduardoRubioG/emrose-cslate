a8ef130e73a39a33818ca3ecb3a2d570145dbf78
Eduardo Rubio <eduardo.rubio.jr85@gmail.com>
2020-07-24 16:54:53 -0400

    ImageBanner performance boost
    Made the Large banner a little more performant so that it doesn't load
    all the images in mobile, if it doesn't need them.

--- src/components/FeaturedImageBannerL/FeaturedImageBannerL.js
+++ src/components/FeaturedImageBannerL/FeaturedImageBannerL.js
@@ -1,25 +1,54 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import Img from "gatsby-image"
 import "./FeaturedImageBannerL.scss"
 
+function getWindowDimensions() {
+  const { innerWidth: width } = window
+  return {
+    width,
+  }
+}
+
+function useWindowDimensions() {
+  const [windowDimensions, setWindowDimensions] = useState(
+    getWindowDimensions()
+  )
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions())
+    }
+    window.addEventListener("resize", handleResize)
+    return () => window.removeEventListener("resize", handleResize)
+  }, [])
+  return windowDimensions
+}
+
 const FeaturedImageBannerL = props => {
-  const centerPiece = props.data.edges[0].node.centerPiece.fluid;
-  const topleft = props.data.edges[0].node.sideImages[0].fluid;
-  const bottomleft = props.data.edges[0].node.sideImages[1].fluid;
-  const topright = props.data.edges[0].node.sideImages[2].fluid;
-  const bottomright = props.data.edges[0].node.sideImages[3].fluid;
+  const { width } = useWindowDimensions()
+  const centerPiece = props.data.edges[0].node.centerPiece.fluid
+  const topleft = props.data.edges[0].node.sideImages[0].fluid
+  const bottomleft = props.data.edges[0].node.sideImages[1].fluid
+  const topright = props.data.edges[0].node.sideImages[2].fluid
+  const bottomright = props.data.edges[0].node.sideImages[3].fluid
+
+  if (width < 860) {
+    console.log("SMALLER")
+  }
+  const bannerImages =
+    width > 860 ? [
+        <Img fluid={topleft} className="top-left" />,
+        <Img fluid={bottomleft} className="bottom-left" />,
+        <Img fluid={centerPiece} className="FeaturedImageBannerL__image cp" />,
+        <Img fluid={topright} className="top-right" />,
+        <Img fluid={bottomright} className="bottom-right" />,
+     ] : [
+        <Img fluid={centerPiece} className="FeaturedImageBannerL__image cp" />
+     ];
 
   return (
     <div className="FeaturedImageBannerL__container">
-      <Img fluid={topleft} className="top-left"/>
-      <Img fluid={bottomleft} className="bottom-left"/>
-      <Img
-        fluid={centerPiece}
-        className="FeaturedImageBannerL__image cp"
-      />
-      <Img fluid={topright} className="top-right"/>
-      <Img fluid={bottomright} className="bottom-right"/>
-    </div>
+   {bannerImages}
+   </div>
   )
 }
 
