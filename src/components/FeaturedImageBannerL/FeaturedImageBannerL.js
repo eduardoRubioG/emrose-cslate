3f75c8faa10205f7552fb527b1e7b5958be79aea
Eduardo Rubio <eduardo.rubio.jr85@gmail.com>
2020-07-24 17:03:46 -0400

    useEffect for SSR issues
    Hot fix

--- src/components/FeaturedImageBannerL/FeaturedImageBannerL.js
+++ src/components/FeaturedImageBannerL/FeaturedImageBannerL.js
@@ -2,28 +2,29 @@ import React, { useState, useEffect } from "react"
 import Img from "gatsby-image"
 import "./FeaturedImageBannerL.scss"
 
-function getWindowDimensions() {
-  const { innerWidth: width } = window
-  return {
-    width,
-  }
-}
-
-function useWindowDimensions() {
-  const [windowDimensions, setWindowDimensions] = useState(
-    getWindowDimensions()
-  )
-  useEffect(() => {
-    function handleResize() {
-      setWindowDimensions(getWindowDimensions())
-    }
-    window.addEventListener("resize", handleResize)
-    return () => window.removeEventListener("resize", handleResize)
-  }, [])
-  return windowDimensions
-}
-
 const FeaturedImageBannerL = props => {
+  function getWindowDimensions() {
+    const { innerWidth: width } = window
+    return {
+      width,
+    }
+  }
+
+  function useWindowDimensions() {
+    const [windowDimensions, setWindowDimensions] = useState(
+      getWindowDimensions()
+    )
+
+    useEffect(() => {
+      function handleResize() {
+        setWindowDimensions(getWindowDimensions())
+      }
+      window.addEventListener("resize", handleResize)
+      return () => window.removeEventListener("resize", handleResize)
+    }, [])
+    return windowDimensions
+  }
+
   const { width } = useWindowDimensions()
   const centerPiece = props.data.edges[0].node.centerPiece.fluid
   const topleft = props.data.edges[0].node.sideImages[0].fluid
@@ -31,25 +32,21 @@ const FeaturedImageBannerL = props => {
   const topright = props.data.edges[0].node.sideImages[2].fluid
   const bottomright = props.data.edges[0].node.sideImages[3].fluid
 
-  if (width < 860) {
-    console.log("SMALLER")
-  }
   const bannerImages =
-    width > 860 ? [
-        <Img fluid={topleft} className="top-left" />,
-        <Img fluid={bottomleft} className="bottom-left" />,
-        <Img fluid={centerPiece} className="FeaturedImageBannerL__image cp" />,
-        <Img fluid={topright} className="top-right" />,
-        <Img fluid={bottomright} className="bottom-right" />,
-     ] : [
-        <Img fluid={centerPiece} className="FeaturedImageBannerL__image cp" />
-     ];
+    width > 860
+      ? [
+          <Img fluid={topleft} className="top-left" />,
+          <Img fluid={bottomleft} className="bottom-left" />,
+          <Img
+            fluid={centerPiece}
+            className="FeaturedImageBannerL__image cp"
+          />,
+          <Img fluid={topright} className="top-right" />,
+          <Img fluid={bottomright} className="bottom-right" />,
+        ]
+      : [<Img fluid={centerPiece} className="FeaturedImageBannerL__image cp" />]
 
-  return (
-    <div className="FeaturedImageBannerL__container">
-   {bannerImages}
-   </div>
-  )
+  return <div className="FeaturedImageBannerL__container">{bannerImages}</div>
 }
 
 export default FeaturedImageBannerL
